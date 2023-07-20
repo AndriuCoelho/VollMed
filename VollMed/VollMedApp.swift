8db4b53c18b4f6e77493df191df7eeaf18309f59
Ândriu <andriu_coelho1993@hotmail.com>
2023-07-20 18:27:39 -0300

    start project

---
+++ VollMed/VollMedApp.swift
@@ -0,0 +1,17 @@
+//
+//  VollMedApp.swift
+//  VollMed
+//
+//  Created by Ândriu F Coelho on 20/07/23.
+//
+
+import SwiftUI
+
+@main
+struct VollMedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
